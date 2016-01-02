0587b51a9a802c5d57640fdb372e099e302690b2
Chrissy LeMaire <clemaire@gmail.com>
2016-01-02 14:36:11 +0100

    Doc clean up

--- CloudFlareDynDns/CloudFlareDynDns.psm1
+++ CloudFlareDynDns/CloudFlareDynDns.psm1
@@ -6,11 +6,10 @@ Function Update-CloudFlareDynamicDns
         Updates specified CloudFlare DNS hostname to the current connection's external IP address using CloudFlare API v4 
 		https://api.cloudflare.com/
 		
-
         .DESCRIPTION
         This module is useful for homelabs. Remember how DynDns used to dynamically update your IP address for free? The functionality provided by this module is similar but updates CloudFlare hosted domains. CloudFlare is free and awesome, and I recommend it, if even for its simplified DNS management and free HTTPS.
 		
-		This should be setup as a scheduled task. I set mine for 5 minutes.
+        This should be setup as a scheduled task. I set mine for 5 minutes.
 
         .PARAMETER Token
         CloudFlare API Token. 
